a4efaa095ab1b3b677dcf81e3a1e492a6df7e03f
hatim <laravel.supersoft@gmail.com>
2024-10-17 18:56:43 +0500

    xzcfhg

--- app/Models/Openrequest.php
+++ app/Models/Openrequest.php
@@ -10,7 +10,7 @@ class Openrequest extends Model
     protected $fillable = [
         'to',
         'from',           
-        'title',
+        'titel',
         'status'
     ];
     protected $table = 'openrequests';
